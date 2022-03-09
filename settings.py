775f80a672cb515bac5dc8424bfd1a54758a6855
Adrià Bronchal <adriaw28@gmail.com>
2022-03-09 12:40:15 +0100

    add

--- settings.py
+++ settings.py
@@ -9,6 +9,14 @@ SESSION_CONFIGS = [
      ),
 ]
 
+ROOMS = [
+    dict(
+        name="PD",
+        display_name='PD',
+        use_secure_urls= False
+    )
+]
+
 # if you set a property in SESSION_CONFIG_DEFAULTS, it will be inherited by all configs
 # in SESSION_CONFIGS, except those that explicitly override it.
 # the session config can be accessed from methods in your apps as self.session.config,
